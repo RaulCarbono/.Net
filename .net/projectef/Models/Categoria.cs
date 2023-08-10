d40baf5a954b59c0d40b342357589d30d8af5dca
RaulCarbono <88171012+RaulCarbono@users.noreply.github.com>
2023-08-10 16:59:36 -0500

    get

--- .net/projectef/Models/Categoria.cs
+++ .net/projectef/Models/Categoria.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
 namespace proyectoef.Models;
 
 public class Categoria
@@ -14,5 +16,6 @@ public class Categoria
 
     public int Peso { get; set; }
 
+    [JsonIgnore]
     public virtual ICollection<Tarea> Tareas { get; set; }
 }
