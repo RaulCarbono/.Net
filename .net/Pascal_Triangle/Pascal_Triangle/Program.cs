2d239ba99c071ba0b4fecf70c7bb8528a6c62fbb
RaulCarbono <88171012+RaulCarbono@users.noreply.github.com>
2023-08-14 15:16:07 -0500

    Pascal_Triangle

---
+++ .net/Pascal_Triangle/Pascal_Triangle/Program.cs
@@ -0,0 +1,43 @@
+﻿// Snamespace pascal_triangle
+using System;
+
+
+   namespace pascal_triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pisos = 0;
+            int[] arreglo = new int[1];
+            Console.WriteLine("Ingrese el numero de pisos");
+            pisos = Convert.ToInt16(Console.ReadLine()); 
+
+            for (int i = 1; i <= pisos; i++)
+            {
+                int[] pascal = new int[i];
+                for (int j = pisos; j < i; j--)
+                {
+                    Console.Write("");
+                }
+                for (int k = 0; k < i; k++)
+                {
+                    if ( k == 0 || k == (i -1))
+                    {
+                        pascal[k] = 1;
+                    }
+                    else
+                    {
+                        pascal[k] = arreglo[k] + arreglo[k - 1];
+                    }
+                    Console.Write("[" + pascal[k] + "]");
+                }
+                arreglo = pascal; 
+                    Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
